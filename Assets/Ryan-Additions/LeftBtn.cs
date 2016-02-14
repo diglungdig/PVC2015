1593cc7340ad2b2a772a93aef8f4e1d9da5dcc55
diglungdig <tachyon.of.tear@gmail.com>
2016-02-14 01:55:03 -0500

    delete old files

--- Assets/Ryan-Additions/LeftBtn.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class LeftBtn : MonoBehaviour {
-    public GameObject sphere;
-	public void DoAction() {
-        sphere.SendMessage("Left");
-    }
-}
