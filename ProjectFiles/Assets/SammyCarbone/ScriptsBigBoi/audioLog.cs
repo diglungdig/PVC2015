31d4498445995af075f53b77d10d64258eb9d185
scarbone98 <scarbon@purdue.lcl>
2016-03-24 19:57:35 -0400

    sammy commit ayyy lmao

---
+++ ProjectFiles/Assets/SammyCarbone/ScriptsBigBoi/audioLog.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class audioLog : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!GetComponent<AudioSource>().isPlaying)
+            {
+                GetComponent<AudioSource>().Play();
+            }
+            else
+            {
+                GetComponent<AudioSource>().Pause();
+            }
+        }
+        Debug.Log("Ayy lmao");
+    }
+}
